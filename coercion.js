f7b4a6e1d2b33a16ffd6a0ccb8911f39cf747bc6
Jeremiah <jeremiahc959@gmail.com>
2022-11-09 10:59:21 -0500

    added an html file

---
+++ coercion.js
@@ -0,0 +1,4 @@
+let a = 7;
+let b = '6';
+let c = a + b;
+console.log('Answer: ' + c);
---
+++ datatypes.js
@@ -0,0 +1,12 @@
+let x = 7; // number //
+console.log(typeof x);
+
+let y = true; // true or false statement (boolean) //
+console.log(typeof y);
+
+let z = 'Hello World'; // string (Any piece of text) //
+console.log(typeof z);
+
+let a;
+console.log(a); // meaning Undefined //
+console.log(typeof a); 
